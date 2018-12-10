f44909c072e089a62a274dd1537b6a7d44a972a0
praveen0509 <praveen.pavan0509@gmail.com>
2018-12-10 20:08:04 +0530

    day5

--- java/day5/pattern.java
+++ java/day5/pattern.java
@@ -1,10 +1,14 @@
 import java.io.*;
+import java.util.*;
 
 public class pattern
 {
   public static void main(String args[])
   {
-    int i,j,n=4;
+    int i,j,n;
+    System.out.println("enter n value:");
+    Scanner ob=new Scanner(System.in);
+    n=ob.nextInt();
     for(i=1;i<=n;i++)
     {
       for(j=1;j<=i*5;j++)
--- java/day5/sequence.java
+++ java/day5/sequence.java
@@ -4,7 +4,10 @@ public class sequence
 {
   public static void main(String args[])
   {
-   int i=1,j=0,n=9;
+   int i=1,j=0,n;
+   System.out.println("enter n value");
+   Scanner ob=new Scanner(System.in);
+   n=ob.nextInt();
    System.out.println(i);
    for(i=2;i<=n;i++)
    {
