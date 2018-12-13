67a993cc0d41cbf5629b5b23e5838116e73edf11
praveen0509 <praveen.pavan0509@gmail.com>
2018-12-13 20:04:16 +0530

    day1312

---
+++ java/Day1312/day1312_strings.java
@@ -0,0 +1,25 @@
+
+public class day1312_strings
+{
+
+	public static void main(String args[])
+	{
+		String s1="Semanticbits";
+		String s2="India";
+		System.out.println("length:"+s1.length());     //length() method for strings
+	                                                  //length for arrays[]
+		System.out.println("charAt(5):-"+s1.charAt(5));
+		System.out.println("s1.concat(s2):-"+s1.concat(s2));
+		System.out.println("s1.compareTo(s2):-"+s1.compareTo(s2));
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
