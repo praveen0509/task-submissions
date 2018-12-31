868fd64a1f75cf9e2f8243e67d0e5a98358eb0ed
praveen0509 <praveen.pavan0509@gmail.com>
2018-12-31 11:38:56 +0530

    pattern modified

--- java/Day6/Pattern.java
+++ java/Day6/Pattern.java
@@ -5,7 +5,7 @@ public class Pattern
 {
 	public static void main(String args[])
 	{
-	   short num,space,star;
+	   short num,space,star=0,store=1;
 	   boolean flag=true;
 	   Scanner ob=new Scanner(System.in);
 	   System.out.println("Enter n value:");
@@ -13,29 +13,20 @@ public class Pattern
 	   
 	   for(short lineNo=1; lineNo<=2*num-1; lineNo++)
 	   {
-		  
-		  if(lineNo<=num)
-		  {
+		  if(lineNo<=num){
 			  star=1;
 			  space=(short)(num - lineNo);
 		  }
-		  else
-		  {
+		  else{
+			  star=store;
 			  space=(short)(lineNo - num); 
 			  star+=2;
-			  if(flag==true)
-			  {
-			   star=(short)(num-1);
-			   flag=false;
-			  }	
+			  store=star;
 		  }
 		  for(; space>0; space--)     //space for loop 
-		  {
 			System.out.print(" ");	  
-			  
-		  }
 		  
-		  for(; star<=lineNo; star++)                      //star printing for loop
+		  for(; star<=lineNo; star++)      //star printing for loop
 			  System.out.print("* ");
 		  System.out.println("");		   
 		   
