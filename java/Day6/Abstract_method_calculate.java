41cd6be3f29fa6bb6efdc5123903bacc60c05470
praveen0509 <praveen.pavan0509@gmail.com>
2019-01-02 19:26:08 +0530

    modified java programs

--- java/Day6/Abstract_method_calculate.java
+++ java/Day6/Abstract_method_calculate.java
@@ -2,5 +2,36 @@ package Day6;
 import java.util.Scanner;
 
 public class Abstract_method_calculate {
+	public static void main(String args[]) {
+		Son sonObj=new Son();
+		sonObj.calculate();		
+	}
 
 }
+
+//parent class
+abstract class Parent {
+	public int id;
+	public String name;
+		 
+	abstract void calculate();
+}
+
+//child class
+class Son extends Parent {
+	public void calculate(){
+		Scanner  ob=new Scanner(System.in);
+		System.out.println("Enter student Id and name:-");
+		id=ob.nextInt();
+		name=ob.next();
+		System.out.println("Student Id="+id+" name="+name);
+	}
+	
+}
+
+/*  output:
+	Enter student Id and name:-
+	104
+	praveen
+	Student Id=104 name=praveen
+*/
